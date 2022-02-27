dbbbb7bf325cbd0469b09962738ee6388c54d78d
vntero <shin.antero@gmail.com>
2022-02-27 12:18:16 +0100

    seeder created successfully

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -1,26 +1,16 @@
-<?php
+<?php          
 
-namespace Database\Factories;
+use Faker\Generator as Faker;          
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
- */
-class ContactFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            'name' => $faker->name,
-            'email' => $faker->email,
-            'phone' => $faker->e164PhoneNumber,
-            'country' => $faker->country,
-        ];
-    }
-}
+$factory->define(App\Contact::class, function (Faker $faker) {         
+    // generate 3 random colors         
+    $colors = collect(range(1, 3))->map(function() use ($faker) { return $faker->colorName; })->toArray();              
+    
+    return [             
+        'name' => $faker->name, 
+        'email' => $faker->email,               
+        'phone' => $faker->e164PhoneNumber,
+        'country' => $faker->country,             
+        'favorites' => ['colors' => $colors],         
+    ];     
+});
